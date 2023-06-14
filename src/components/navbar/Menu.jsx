8e254cdaac2c5d89b3b217a3eabcac3ad39b3b79
lakivr3 <127746116+lakivr3@users.noreply.github.com>
2023-06-14 20:17:14 +0200

    Initial commit

---
+++ src/components/navbar/Menu.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Menu = () => {
+  <div className="gpt3__navbar-links_container">
+    <p>
+      <a href="#home">Home</a>
+    </p>
+    <p>
+      <a href="#wgpt3">What is GPT-3?</a>
+    </p>
+    <p>
+      <a href="#possibility">Open AI</a>
+    </p>
+    <p>
+      <a href="#features">Case Studies</a>
+    </p>
+    <p>
+      <a href="#blog">Library</a>
+    </p>
+  </div>;
+};
+
+export default Menu;
